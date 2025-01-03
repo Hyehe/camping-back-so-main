9579e294be4997dfd3a39a7f629f9166a559fe99
Hyehe <khl99122412@gmail.com>
2025-01-03 20:04:19 +0900

    first commit

---
+++ src/main/java/com/ict/camping/domain/regularMeeting/vo/RegularMeetingBoardImageVO.java
@@ -0,0 +1,12 @@
+package com.ict.camping.domain.regularMeeting.vo;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class RegularMeetingBoardImageVO {
+  private int image_idx, image_url;
+}
